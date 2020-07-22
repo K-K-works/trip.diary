597d177a818a77f1aba353185c8f03a5a4e5775b
K-K-works <67718810+K-K-works@users.noreply.github.com>
2020-07-22 23:54:08 +0900

    Create slick.edit.js

---
+++ slick.edit.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function(){
+    $('.slick').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      cssEase: 'case-in-out',
+      infinite: true,
+      swipe: true,
+      fade: true,
+      arrows: false,
+    });
+  });
