9edd542243329fb020ce985ef84ec129e87f0fcb
K-K-works <67718810+K-K-works@users.noreply.github.com>
2020-07-27 22:50:02 +0900

    Update slick.edit.js

--- slick.edit.js
+++ slick.edit.js
@@ -38,4 +38,27 @@
       $('header').height(hSize);
     });
     
+    $('#menu-open').click(function(){
+      $(this).removeClass('active');
+      $('#menu-wrapper').fadeIn();
+      $('.close').addClass('active');
+    });
+    $('#menu-close').click(function(){
+      $('.close').removeClass('active');
+      $('#menu-wrapper').fadeOut();
+      $('#menu-open').addClass('active');
+    });
+
+
+    var topBtn = $('#menu-open');
+   topBtn.hide();
+   //スクロールが100に達したらボタン表示
+   $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+           topBtn.fadeIn();
+       } else {
+           topBtn.fadeOut();
+       }
+   });
+    
   });
